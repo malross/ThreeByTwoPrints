6cffe81c8ff62dbd622e2b252cef2a2ec218aff6
Mal Ross <malross@gmail.com>
2015-10-21 21:52:52 +0100

    Initial commit
    Only a very basic, and incomplete, UI at the moment, allowing you to
    select photos to open (via a standard file dialog) and then click on a
    photo to see a bigger version. No exports yet, or owt.

---
+++ PhotoViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ThreeByTwoPrints
+{
+    public class PhotoViewModel : ReactiveObject
+    {
+        private Lazy<BitmapImage> image;
+        private readonly string filePath;
+        private int copies;
+        private readonly ReactiveCommand<object> addCopyCommand;
+        private readonly ReactiveCommand<object> removeCopyCommand;
+
+        public PhotoViewModel(string filePath)
+        {
+            this.filePath = filePath;
+            copies = 1;
+
+            image = new Lazy<BitmapImage>(() => new BitmapImage(new Uri(filePath)));
+
+            addCopyCommand = ReactiveCommand.Create();
+            addCopyCommand.Subscribe(_ => Copies = Copies + 1);
+            removeCopyCommand = ReactiveCommand.Create();
+            removeCopyCommand.Subscribe(_ => Copies = Math.Max(1, Copies - 1));
+        }
+
+        public string FilePath { get { return filePath; } }
+
+        public ImageSource Image { get { return image.Value; } }
+
+        public int Copies
+        {
+            get { return copies; }
+            set { this.RaiseAndSetIfChanged(ref copies, value); }
+        }
+
+        public ICommand AddCopyCommand { get { return addCopyCommand; } }
+        public ICommand RemoveCopyCommand { get { return removeCopyCommand; } }
+    }
+}
---
+++ PrintsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace ThreeByTwoPrints
+{
+    public class PrintsViewModel : ReactiveObject
+    {
+        private readonly List<PhotoViewModel> photos = new List<PhotoViewModel>();
+        private PhotoViewModel selectedPhoto;
+        private readonly ICollectionView collectionView;
+        private readonly ReactiveCommand<object> openPhotosCommand;
+
+        public PrintsViewModel()
+        {
+            collectionView = CollectionViewSource.GetDefaultView(photos);
+
+            openPhotosCommand = ReactiveCommand.Create();
+            openPhotosCommand.Subscribe(_ => OpenPhotos());
+        }
+
+        private void OpenPhotos()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Photos|*.jpg;*.jpeg|All Files|*.*";
+            dialog.DefaultExt = ".jpg";
+            dialog.Title = "Open Photos";
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog() == true)
+            {
+                var newFilePaths = dialog.FileNames.Except(photos.Select(p => p.FilePath));
+                photos.AddRange(newFilePaths.Select(p => new PhotoViewModel(p)));
+                collectionView.Refresh();
+            }
+        }
+
+        public ICollectionView Photos { get { return collectionView; } }
+
+        public PhotoViewModel SelectedPhoto
+        {
+            get { return selectedPhoto; }
+            set { this.RaiseAndSetIfChanged(ref selectedPhoto, value); }
+        }
+
+        public ICommand OpenPhotosCommand { get { return openPhotosCommand; } }
+    }
+}
